65b7ed212bea7abed3d19104a2ea32782fa015d7
Cameron Fuller <franksjacket@yahoo.com>
2016-02-24 13:31:16 -0800

    Check-in page controller setup and css working.

--- src/app/checkin/checkin.controller.js
+++ src/app/checkin/checkin.controller.js
@@ -6,7 +6,45 @@
     .controller('CheckinController', CheckinController);
 
   /** @ngInject */
-  function CheckinController($scope) {
+  function CheckinController($scope, $http) {
+     var JOBS_GET_URL = "https://red-wdp-api.herokuapp.com/api/mars/jobs";
+     var COLONIST_POST_URL = 'https://red-wdp-api.herokuapp.com/api/mars/colonists';
+
+     $scope.colonist = {};
+
+   //   This is the GET API request setup.
+     $http({
+        method: 'Get',
+        url: JOBS_GET_URL
+     }).then(function(response){
+        console.log(response);
+        $scope.jobs = response.data.jobs;
+     }, function(error){
+        console.log(error);
+      //   Handle it yourself, jerk!
+     });
+     // This is the POST API setup.
+     $scope.login = function (event) {
+        event.preventDefault();
+
+        $http({
+           method:'POST',
+           url: COLONIST_POST_URL,
+           data: { "colonist":$scope.colonist}
+        })
+        .then(function(response) {
+           console.log(response)
+         //   navigate to next page.
+        }, function(error) {
+           console.log(error);
+        });
+
+     };
+
+
+
+
+
   }
 
 })();
